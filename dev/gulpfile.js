d74849c137659b38552c1c6950555469ad32d773
goodstar <g00dst4r.1207@gmail.com>
2025-03-05 09:09:20 +0900

    add: 機能追加

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -11,16 +11,14 @@ const minimist = require('minimist');
 const os = require('os');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
-
-// 画像圧縮用のプラグイン
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const mozjpeg = require('imagemin-mozjpeg');
 const imageminGif = require('imagemin-gifsicle');
 const imageminSvg = require('imagemin-svgo');
-
-// キャッシュプラグイン
 const cached = require('gulp-cached');
+const rsync = require('gulp-rsync');
+const rename = require('gulp-rename');
 
 // コマンドラインオプションの設定
 const options = minimist(process.argv.slice(2), {
@@ -37,6 +35,10 @@ const paths = {
     src: srcBase + '/images/**/*.{jpg,jpeg,png,gif,svg}',
     dist: distBase + '/images'
   },
+  video: {
+    src: srcBase + '/video/**/*.{mp4,webm,ogg}',
+    dist: distBase + '/video'
+  },
   sass: {
     src: srcBase + '/sass/**/*.scss',
     dist: distBase + '/css'
@@ -50,20 +52,31 @@ const paths = {
   }
 };
 
-// SCSS コンパイルタスク（gulp-cached はパーシャルの影響などを考慮して使用しません）
+// Video タスク
+const videoMin = () => {
+  return src(paths.video.src)
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
+    .pipe(cached('videoMin'))
+    .pipe(dest(paths.video.dist))
+    .pipe(browserSync.stream());
+};
+
+// SCSS コンパイルタスク
 const compileSass = () => {
   return src(paths.sass.src)
     .pipe(plumber({ errorHandler: notify.onError('Sass Error: <%= error.message %>') }))
-    .pipe(gulpSass({ outputStyle: 'expanded' }).on('error', gulpSass.logError))
+    .pipe(gulpSass({ outputStyle: 'compressed' }).on('error', gulpSass.logError))
     .pipe(postcss([autoprefixer()]))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(dest(paths.sass.dist))
     .pipe(browserSync.stream());
 };
 
-// JS の結合・minify タスク（変更があったファイルのみ処理）
+// JS の結合・minify タスク
 const minifyJs = () => {
   return src(paths.js.src)
-    .pipe(cached('scripts'))
     .pipe(plumber({ errorHandler: notify.onError('JS Error: <%= error.message %>') }))
     .pipe(concat('script.min.js'))
     .pipe(terser())
@@ -86,11 +99,10 @@ const imageMin = () => {
     .pipe(dest(paths.images.dist));
 };
 
-// BrowserSync タスク（ネットワーク上の IP アドレスを自動取得して外部アクセスを有効に）
+// BrowserSync タスク（ネットワーク上のIPアドレスを自動取得して外部アクセスを有効に）
 const browserSyncTask = (done) => {
   const interfaces = os.networkInterfaces();
   let ipAddress;
-
   for (const iface of Object.values(interfaces)) {
     for (const alias of iface) {
       if (alias.family === 'IPv4' && !alias.internal) {
@@ -100,7 +112,6 @@ const browserSyncTask = (done) => {
     }
     if (ipAddress) break;
   }
-
   browserSync.init({
     host: ipAddress,
     open: 'external',
@@ -122,13 +133,55 @@ const watchFiles = () => {
   watch(paths.sass.src, compileSass);
   watch(paths.js.src, minifyJs);
   watch(paths.images.src, series(imageMin, reload));
+  watch(paths.video.src, series(videoMin, reload));
   watch(paths.php.src).on('change', browserSync.reload);
 };
 
-// デフォルトタスク
-exports.default = series(compileSass, minifyJs, imageMin, browserSyncTask, watchFiles);
 
-// 必要に応じて個別タスクとしてもエクスポート可能
+// 除外パターンの設定
+const excludePatterns = ['.DS_Store', 'dev', 'node_modules', '.git'];
+
+// テスト環境へのデプロイタスク
+const deploytest = () => {
+  return src(['../**', '!../dev/**', '!../node_modules/**', '!../.git/**'])
+    .pipe(rsync({
+      root: '../',
+      hostname: 'xgvcdevelop',
+      destination: '/home/gvcdevelop/gvcdevelop.xsrv.jp/public_html/sk-tire-service/wp-content/themes/sk-tire-service',
+      ssh: true,
+      archive: true,
+      compress: true,
+      recursive: true,
+      clean: true,
+      exclude: excludePatterns
+    }));
+};
+
+// 本番環境へのデプロイタスク
+const deployprod = () => {
+  return src(['../**', '!../dev/**', '!../node_modules/**', '!../.git/**'])
+    .pipe(rsync({
+      root: '../',
+      hostname: 'xserver2',
+      destination: '/home/gvc2/sk-tire-service.com/public_html/wp-content/themes/sk-tire-service',
+      ssh: true,
+      archive: true,
+      compress: true,
+      recursive: true,
+      clean: true,
+      exclude: excludePatterns
+    }));
+};
+
+// デフォルトタスク（SCSS, JS, 画像圧縮、BrowserSync、監視）
+exports.default = series(compileSass, minifyJs, imageMin, videoMin, browserSyncTask, watchFiles);
+
+// デプロイタスク（必要に応じて両方実行）
+exports.deploy = series(deploytest, deployprod);
+exports.deploytest = deploytest;
+exports.deployprod = deployprod;
+
+// 必要に応じて個別タスクもエクスポート可能
 exports.compileSass = compileSass;
 exports.minifyJs = minifyJs;
 exports.imageMin = imageMin;
